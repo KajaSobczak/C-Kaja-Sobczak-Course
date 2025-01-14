09076a520fe57fc4a77c4cb2380110796d0173c7
KajaSobczak <kaja.sobczak@cdprojektred.com>
2025-01-14 17:50:15 +0100

    Setting Up the project + Character Class

---
+++ Source/CPPKajaSobczakCourse/CPPKajaSobczakCourse.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CPPKajaSobczakCourse.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CPPKajaSobczakCourse, "CPPKajaSobczakCourse" );
